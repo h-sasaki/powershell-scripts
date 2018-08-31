8a4ee6304b38e226260a60293e84a7673037459c
h-sasaki <hi.sas31@gmail.com>
2018-08-31 18:54:23 +0900

    modify name of variable

--- join_domain.ps1
+++ join_domain.ps1
@@ -1,5 +1,5 @@
 Param (
-    [Parameter(Mandatory=$True)][string]$server,
+    [Parameter(Mandatory=$True)][string]$vcenter,
     [Parameter(Mandatory=$True)][string]$domain,
     [Parameter(Mandatory=$True)][string]$domain_user,
     [Parameter(Mandatory=$True)][SecureString]$domain_password
@@ -7,10 +7,12 @@ Param (
 
 Try {
     Import-Module VMware.PowerCLI
-    Connect-VIServer -Server $server
+    Connect-VIServer -Server $vcenter
 } Catch {
     Write-Host "Error found during preparation : `n$_"
     Break
 }
 
 Get-VMHost | Get-VMHostAuthentication | Set-VMHostAuthentication -Domain $domain -JoinDomain -Username $domain_user -Password $domain_password -Confirm:$false
+
+Disconnect-VIServer -Server $vcenter -Confirm:$False
